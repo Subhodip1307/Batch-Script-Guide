083158f4a3cead20531227a0bfdb1021bd9a4185
Subhodip Sardar <sardarsubhodip857@gmail.com>
2023-08-06 21:02:18 +0530

    Taking Input file added

---
+++ Examples bat/Taking_input.bat
@@ -0,0 +1,10 @@
+rem Here I have use @echo off so you are suggested to run the same code without @echo off so increase your understanding 
+@echo off
+set /p name= What is your name 
+set /p age= what is your age  
+rem here we have ask two questions now we will go further and learn to print it
+
+echo Your name is %name%
+echo your age is %age%
+pause 
+  
